59cd5b04b349d5aa7786307c9d108e02bcd486e1
marcel-judth <marjudth@gmail.com>
2019-05-05 08:51:26 -0700

    update

--- 008_Books_JSF/src/java/DataAccess/Database.java
+++ 008_Books_JSF/src/java/DataAccess/Database.java
@@ -67,7 +67,7 @@ public class Database {
     
     private static void connect() throws SQLException{
         DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
-        connection = DriverManager.getConnection(DB_URL ,USER,PASS);
+        connection = DriverManager.getConnection(DB_URL_EXT ,USER,PASS);
     }
     
     public static User login(String username, String password) throws SQLException{
